a02832d25d1c65656b034e4d0cbf5e0803604d09
msiiqbal <msimdshoiebiqubal7147@gmail.com>
2021-06-01 19:57:36 +0530

    basic components added

---
+++ playstore/src/app/projectbody/projectbody.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projectbody',
+  templateUrl: './projectbody.component.html',
+  styleUrls: ['./projectbody.component.css']
+})
+export class ProjectbodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
