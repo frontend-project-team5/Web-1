377552246be52d9f0bf6c4579405e68c24f9288e
sowmya <sowmya2431@gmail.com>
2021-06-02 20:34:38 +0530

    header and routing done

--- playstore/src/app/app-routing.module.ts
+++ playstore/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MyappsComponent } from './myapps/myapps.component';
+import { MybooksComponent } from './mybooks/mybooks.component';
+import { MymoviesComponent } from './mymovies/mymovies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"myapps",component:MyappsComponent},
+  {path:"mybooks",component:MybooksComponent},
+  {path:"mymovies",component:MymoviesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
