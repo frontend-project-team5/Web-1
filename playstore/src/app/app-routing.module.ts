529d84655899a0b2aaf45aa648de0c80f190f40c
msiiqbal <msimdshoiebiqubal7147@gmail.com>
2021-06-03 12:12:51 +0530

    signin done

--- playstore/src/app/app-routing.module.ts
+++ playstore/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { MyappsComponent } from './myapps/myapps.component';
 import { MybooksComponent } from './mybooks/mybooks.component';
 import { MymoviesComponent } from './mymovies/mymovies.component';
+import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
   {path:"myapps",component:MyappsComponent},
   {path:"mybooks",component:MybooksComponent},
-  {path:"mymovies",component:MymoviesComponent}
+  {path:"mymovies",component:MymoviesComponent},
+  {path:"signin",component:SigninComponent}
 ];
 
 @NgModule({
